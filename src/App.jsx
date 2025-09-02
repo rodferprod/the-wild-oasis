26689a91c948b1d2418b404ef07f21588065bf0c
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-09-02 00:25:21 -0300

    First commit The Wild Oasis

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles";
+
+const StyledApp = styled.main`
+    padding: 20px;
+    background-color: orangered;
+`;
+
+const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+    background: yellow;
+`;
+
+const Button = styled.button`
+    font-size: 1.4rem;
+    padding: 1.2rem 1.6rem;
+    font-weight: 500;
+    border: none;
+    border-radius: 7px;
+    background-color: purple;
+    color: white;
+    margin: 20px;
+    cursor: pointer;
+`;
+
+const Input = styled.input`
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    padding: 0.8rem 1.2rem;
+`;
+function App() {
+    return (
+        <>
+            <GlobalStyles />
+            <StyledApp>
+                <H1>The Wild Oasis</H1>
+                <Button onClick={() => alert('Check in')}>Check in</Button>
+                <Button onClick={() => alert('Check out')}>Check out</Button>
+                <Input type="number" placeholder="Number of guests" />
+                <Input type="number" placeholder="Number of guests" />
+            </StyledApp>
+        </>
+    )
+}
+
+export default App
