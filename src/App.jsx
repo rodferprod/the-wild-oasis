746dc2325d74f845e87d206c6b76ee96d1adba2e
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-09-02 23:55:04 -0300

    feat: styled-components global styles, props and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,24 @@
 import styled from "styled-components"
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.main`
     padding: 20px;
     background-color: orangered;
 `;
 
-const H1 = styled.h1`
-    font-size: 30px;
-    font-weight: 600;
-    background: yellow;
-`;
-
-const Button = styled.button`
-    font-size: 1.4rem;
-    padding: 1.2rem 1.6rem;
-    font-weight: 500;
-    border: none;
-    border-radius: 7px;
-    background-color: purple;
-    color: white;
-    margin: 20px;
-    cursor: pointer;
-`;
-
-const Input = styled.input`
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    padding: 0.8rem 1.2rem;
-`;
 function App() {
     return (
         <>
             <GlobalStyles />
             <StyledApp>
-                <H1>The Wild Oasis</H1>
+                <Heading as='h1'>The Wild Oasis</Heading>
+                <Heading as='h2'>Check in and out</Heading>
                 <Button onClick={() => alert('Check in')}>Check in</Button>
                 <Button onClick={() => alert('Check out')}>Check out</Button>
+                <Heading as='h3'>Form</Heading>
                 <Input type="number" placeholder="Number of guests" />
                 <Input type="number" placeholder="Number of guests" />
             </StyledApp>
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,14 @@
+import styled, { css } from "styled-components";
+
+const Heading = styled.h1`
+
+    ${props => props.as === 'h1' && css`font-size: 4rem;`}
+    ${props => props.as === 'h2' && css`font-size: 3rem;`}
+    ${props => props.as === 'h3' && css`font-size: 2rem;`}
+
+    font-weight: 600;
+    background: yellow;
+    text-align: center;
+`;
+
+export default Heading;
