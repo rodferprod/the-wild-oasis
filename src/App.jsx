d85c967a06a123ad4ff497451d3d7146dada5ca9
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-09-06 00:25:24 -0300

    feat: Setting Up React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import GlobalStyles from "./styles/GlobalStyles"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 import Dashboard from "./pages/Dashboard.jsx"
 import Bookings from "./pages/Bookings.jsx"
@@ -10,9 +12,18 @@ import Account from "./pages/Account.jsx"
 import Login from "./pages/Login.jsx"
 import PageNotFound from "./pages/PageNotFound.jsx"
 import AppLayout from "./ui/AppLayout.jsx"
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60 * 1000,
+        },
+    },
+});
 function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
             <GlobalStyles />
             <BrowserRouter>
                 <Routes>
@@ -29,7 +40,7 @@ function App() {
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
             </BrowserRouter>
-        </>
+        </QueryClientProvider>
     )
 }
 
