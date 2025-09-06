8838c4561b9357e6554258d01fafac3c0d2bb01b
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-09-05 23:52:42 -0300

    feat: Supabase - Connecting and getting info from cabins table

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://fbpqhvjpercsxpllcnqw.supabase.co";
+// supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZicHFodmpwZXJjc3hwbGxjbnF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwMTkzOTcsImV4cCI6MjA3MjU5NTM5N30.z-4gLFDH9mqbk5pUdHXCZALYGQqRVNy2D4G9NPovEdM";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
