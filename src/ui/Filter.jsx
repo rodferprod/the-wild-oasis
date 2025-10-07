547e61fdc7752e1cb8171cce6a3ed8fffc1de5e5
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-10-07 16:15:54 -0300

    feat: Client-side Filtering Cabins by discount

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 const StyledFilter = styled.div`
@@ -15,8 +16,8 @@ const FilterButton = styled.button`
   border: none;
 
   ${(props) =>
-    props.active &&
-    css`
+        props.active &&
+        css`
       background-color: var(--color-brand-600);
       color: var(--color-brand-50);
     `}
@@ -33,3 +34,29 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+function Filter({ filterField, options }) {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const activeFilter = searchParams.get(filterField) || options.at(0).value;
+
+    function handleClick(value) {
+        searchParams.set(filterField, value);
+        setSearchParams(searchParams);
+    }
+
+    return (
+        <StyledFilter>
+            {options.map((opt) => (
+                <FilterButton
+                    key={opt.value}
+                    onClick={() => handleClick(opt.value)}
+                    active={activeFilter === opt.value}
+                >
+                    {opt.label}
+                </FilterButton>
+            ))}
+        </StyledFilter>
+    )
+}
+
+export default Filter
